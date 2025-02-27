413a25d015bb845230f8b21ce082f130cb2bf73f
Niki N <mcnoff@live.com>
2025-02-27 16:01:32 +0200

    refactoring 'user-data-editing'

--- src/ui/user/user-data-editing.tsx
+++ src/ui/user/user-data-editing.tsx
@@ -1,104 +1,128 @@
 import { signout } from '@/lib/actions'
-import DoubleArrowIcon from '@mui/icons-material/DoubleArrow'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
 import LogoutIcon from '@mui/icons-material/Logout'
 import PersonRemoveIcon from '@mui/icons-material/PersonRemove'
+import { Box, Button } from '@mui/material'
 import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
+import List from '@mui/material/List'
+import ListItem, { ListItemProps } from '@mui/material/ListItem'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
-import MenuItem from '@mui/material/MenuItem'
-import MenuList from '@mui/material/MenuList'
 import Stack from '@mui/material/Stack'
 import Link from 'next/link'
+import { ReactNode } from 'react'
 
 export default function UserDataEditing() {
   return (
-    <Stack
-      direction={{ xs: 'column', sm: 'row' }}
-      spacing={5}
-      sx={{ display: 'flex', justifyContent: 'space-around', mt: '3vh' }}
-    >
-      <Card
-        raised
-        sx={{ display: 'flex', justifyContent: 'center', width: { sm: '25%' } }}
-      >
-        <CardActions>
-          <MenuList>
-            <MenuItem
-              component={Link}
-              href='/user/edit-name'
-            >
-              <ListItemIcon>
-                <DoubleArrowIcon fontSize='small' />
-              </ListItemIcon>
-              <ListItemText primary='Edit user name' />
-            </MenuItem>
-            <MenuItem
-              component={Link}
-              href='/user/edit-email'
-            >
-              <ListItemIcon>
-                <DoubleArrowIcon fontSize='small' />
-              </ListItemIcon>
-              <ListItemText primary='Edit email' />
-            </MenuItem>
-          </MenuList>
-        </CardActions>
-      </Card>
-      <Card
-        raised
-        sx={{ display: 'flex', justifyContent: 'center', width: { sm: '25%' } }}
-      >
-        <CardActions>
-          <MenuList
-            component='form'
-            action={signout}
-          >
-            <MenuItem
-              component={Link}
-              href='/user/edit-password'
-            >
-              <ListItemIcon>
-                <DoubleArrowIcon fontSize='small' />
-              </ListItemIcon>
-              <ListItemText primary='Reset password' />
-            </MenuItem>
-            <MenuItem
-              component='button'
-              type='submit'
-            >
-              <ListItemIcon>
-                <LogoutIcon fontSize='small' />
-              </ListItemIcon>
-              <ListItemText primary='Sign out' />
-            </MenuItem>
-          </MenuList>
-        </CardActions>
-      </Card>
-      <Card
-        raised
-        sx={{ display: 'flex', justifyContent: 'center', width: { sm: '25%' } }}
-      >
-        <CardActions>
-          <MenuList>
-            <MenuItem
-              component={Link}
-              href='/user/delete'
-            >
-              <ListItemIcon>
-                <PersonRemoveIcon
-                  fontSize='small'
-                  color='warning'
-                />
-              </ListItemIcon>
-              <ListItemText
-                primary='Delete account'
-                slotProps={{ primary: { color: 'warning' } }}
-              />
-            </MenuItem>
-          </MenuList>
-        </CardActions>
-      </Card>
+    <Stack {...getStackProps()}>
+      <MyCard>
+        <ListItem {...getListItemProps(Link, '/user/edit-name')}>
+          <ListItemText primary='Edit user name' />
+          <MyListItemIcon>
+            <ChevronRightIcon fontSize='small' />
+          </MyListItemIcon>
+        </ListItem>
+        <ListItem {...getListItemProps(Link, '/user/edit-email')}>
+          <ListItemText primary='Edit email' />
+          <MyListItemIcon>
+            <ChevronRightIcon fontSize='small' />
+          </MyListItemIcon>
+        </ListItem>
+      </MyCard>
+      <MyCard>
+        <Box
+          component='form'
+          action={signout}
+        >
+          <ListItem {...getListItemProps(Link, '/user/edit-password')}>
+            <ListItemText primary='Reset password' />
+            <MyListItemIcon>
+              <ChevronRightIcon fontSize='small' />
+            </MyListItemIcon>
+          </ListItem>
+          <ListItem {...getListItemProps(Button, undefined, 'none', 'submit')}>
+            <ListItemText
+              primary='Sign out'
+              slotProps={{
+                primary: {
+                  align: 'left',
+                },
+              }}
+            />
+            <MyListItemIcon>
+              <LogoutIcon fontSize='small' />
+            </MyListItemIcon>
+          </ListItem>
+        </Box>
+      </MyCard>
+      <MyCard>
+        <ListItem {...getListItemProps(Link, '/user/delete')}>
+          <ListItemText
+            primary='Delete account'
+            slotProps={{ primary: { color: 'warning' } }}
+          />
+          <MyListItemIcon>
+            <PersonRemoveIcon
+              fontSize='small'
+              color='warning'
+            />
+          </MyListItemIcon>
+        </ListItem>
+      </MyCard>
     </Stack>
   )
+}
+
+function MyCard({ children }: MyElementsProps) {
+  return (
+    <Card
+      component='nav'
+      variant='outlined'
+      sx={{ display: 'flex', width: { sm: '25%' } }}
+    >
+      <CardActions sx={{ width: '100%' }}>
+        <List sx={{ width: '100%' }}>{children}</List>
+      </CardActions>
+    </Card>
+  )
+}
+
+function MyListItemIcon({ children }: MyElementsProps) {
+  return (
+    <ListItemIcon
+      sx={{
+        display: 'flex',
+        justifyContent: 'flex-end',
+      }}
+    >
+      {children}
+    </ListItemIcon>
+  )
+}
+
+const getStackProps = () => ({
+  direction: { xs: 'column', sm: 'row' } as const,
+  spacing: 5,
+  sx: { display: 'flex', justifyContent: 'space-around', mt: '3vh' },
+})
+
+const getListItemProps = (
+  component?: typeof Link | typeof Button,
+  href?: string,
+  textTransform?: 'none',
+  type?: 'submit',
+): ListItemProps => ({
+  component,
+  ...(href && { href }),
+  ...(type && { type }),
+  sx: {
+    width: '100%',
+    pr: 0,
+    ...(textTransform && { textTransform }),
+  },
+})
+
+interface MyElementsProps {
+  children: ReactNode
 }
